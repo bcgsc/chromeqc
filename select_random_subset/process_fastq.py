8fd547a41a99249327dc2b4d8998fc253865e624
Hamid <hyounesy@gmail.com>
2017-10-20 17:08:28 -0700

    statistics on populations of barcodes

--- select_random_subset/process_fastq.py
+++ select_random_subset/process_fastq.py
@@ -3,55 +3,110 @@
 # how many reads-pairs to process from fastq
 
 import gzip
+import matplotlib.pyplot as plt
 # from Bio import SeqIO
 
-# read the white listed barcodes (
-# barcodes are extracted from longranger tool:
-# longranger-2.1.6/longranger-cs/2.1.6/tenkit/lib/python/tenkit/barcodes/4M-with-alts-february-2016.txt
-barcodes = {}
-print("Reading barcodes ...")
-with open("data/barcode.txt", "r") as f:
-    for line in f.readlines():
-        barcode = line[:-1]
-        if barcode in barcodes:
-            print("duplicate barcode: {}", barcode)
-        barcodes[barcode] = 0
-print("[Done]. {} records".format(len(barcodes)))
+class ProcessFastQBarCodes:
+    """
 
-# process the fastq file
-unmatched_barcodes = {}
-num_unmatched_barcodes = 0
-print("Reading fastq file ...")
-read_index = 0
-prev_read_name = None
-with gzip.open('data/read-RA_si-GAGTTAGT_lane-001-chunk-0002.fastq.gz') as fq:
-    while True:
-        try:
-            read_name = fq.readline().decode("utf-8")[:-1].split(' ')[0]
-            read_seq = fq.readline().decode("utf-8")[:-1]
-            _ = fq.readline()
-            read_qual = fq.readline().decode("utf-8")[:-1]
-            is_read1 = read_index % 2 == 0
-            if is_read1:
-                read_barcode = str(read_seq[:16])
-                read_barcode_count = barcodes.get(read_barcode, -1)
-                if read_barcode_count == -1:
-                    unmatched_barcodes[read_barcode] = unmatched_barcodes.get(read_barcode, 0) + 1
-                    num_unmatched_barcodes += 1
-                else:
-                    barcodes[read_barcode_count] = read_barcode_count + 1
-            else: # read2
-                if read_name != prev_read_name:
-                    print("Mismatched reads at read index {}".format(read_index))
-            prev_read_name = read_name
-            read_index += 1
-            if read_index % 100000 == 0:
-                print("Processed {} reads. found {} unmatched barcodes ({} unique)".format(
-                    read_index, num_unmatched_barcodes, len(unmatched_barcodes)))
-                break
-        except EOFError:
-            pass
-print("Total {} reads. found {} unmatched barcodes ({} unique)".format(
-    read_index, num_unmatched_barcodes, len(unmatched_barcodes)))
+    """
+    def __init__(self):
+        self.standard_barcodes = {}  # dict of the white listed barcodes mapped to read-pair count
+        self.unmatched_barcodes = {}
+        self.num_unmatched_barcodes = 0
+
+    def read_standard_barcodes(self, barcode_filename):
+        """
+        Read the white listed barcodes.
+        Args:
+            barcode_filename: standard barcodes are extracted from longranger tool:
+            longranger-2.1.6/longranger-cs/2.1.6/tenkit/lib/python/tenkit/barcodes/4M-with-alts-february-2016.txt
+        """
+        print('Reading standard (white listed) barcodes ...')
+        with open(barcode_filename, 'r') as f:
+            for line in f.readlines():
+                barcode = line[:-1]
+                if barcode in self.standard_barcodes:
+                    print('duplicate barcode: {}', barcode)
+                self.standard_barcodes[barcode] = 0
+
+        print('[Done]. read {} records'.format(len(self.standard_barcodes)))
+
+    def process_fastq(self, fastq_filename, max_read_pairs=100000):
+        # process the fastq file
+        print('Reading fastq file: {} ...'.format(fastq_filename))
+        read_index = 0
+        prev_read_name = None
+        with gzip.open(fastq_filename) as fq:
+            while True:
+                try:
+                    read_name = fq.readline().decode("utf-8")[:-1].split(' ')[0]
+                    read_seq = fq.readline().decode("utf-8")[:-1]
+                    _ = fq.readline()
+                    read_qual = fq.readline().decode("utf-8")[:-1]
+                    is_read1 = read_index % 2 == 0
+                    if is_read1:
+                        read_barcode = str(read_seq[:16])
+                        read_barcode_count = self.standard_barcodes.get(read_barcode, -1)
+                        if read_barcode_count == -1:
+                            self.unmatched_barcodes[read_barcode] = self.unmatched_barcodes.get(read_barcode, 0) + 1
+                            self.num_unmatched_barcodes += 1
+                        else:
+                            self.standard_barcodes[read_barcode] = read_barcode_count + 1
+                    else: # read2
+                        if read_name != prev_read_name:
+                            print("Mismatched reads at read index {}".format(read_index))
+                    prev_read_name = read_name
+                    read_index += 1
+                    if read_index % 100000 == 0:
+                        print("Processed {} pair of reads. found {}({}%) unmatched barcodes ({} unique)".format(
+                            read_index / 2,
+                            self.num_unmatched_barcodes,
+                            round(2 * 100.0 * self.num_unmatched_barcodes / read_index, 1),
+                            len(self.unmatched_barcodes)))
+                    if max_read_pairs > 0 and read_index/2 >= max_read_pairs:
+                        break
+                except EOFError:
+                    pass
+
+        print("Total: {} pair of reads. found {}({}%) unmatched barcodes ({} unique)".format(
+            read_index / 2,
+            self.num_unmatched_barcodes,
+            round(2 * 100.0 * self.num_unmatched_barcodes / read_index, 1),
+            len(self.unmatched_barcodes)))
+        # create histograms
+        self.plot_histogram_of_counts(self.standard_barcodes, filename='select_random_subset/standard_barcodes.pdf',
+                                      title='population count for standard barcodes for {} read pairs'.format(max_read_pairs))
+        self.plot_histogram_of_counts(self.unmatched_barcodes, filename='select_random_subset/unmatched_barcodes.pdf',
+                                      title='population counts for unmatched barcodes for {} read pairs'.format(max_read_pairs))
+
+    @staticmethod
+    def plot_histogram_of_counts(dict_counts, filename, title = '', remove_zeros=True):
+        sorted_list = [x for x in sorted(list(dict_counts.items()), key=lambda x: x[1], reverse=True) if x[1] > 0]
+        if remove_zeros:
+            sorted_list = [x for x in sorted_list if x[1] > 0]
+
+        max_count = max([x[1] for x in sorted_list])
+        min_count = min([x[1] for x in sorted_list])
+        count_hist = [0] * (max_count - min_count + 1)
+        for x in sorted_list:
+            count_hist[x[1] - min_count] += 1
+
+        print(title)
+        print('\n'.join(['{}: {}'.format(i+min_count, j) for i, j in enumerate(count_hist)]))
+
+        plt.style.use('seaborn-whitegrid')
+        plt.plot(range(min_count, max_count + 1), count_hist)
+        plt.xlabel('population')
+        plt.ylabel('#per population')
+        plt.title(title)
+        plt.savefig(filename)
+        plt.clf()
 
 
+if __name__ == '__main__':
+    barcode_filename = 'data/barcode.txt'
+    fastq_filename = 'data/read-RA_si-GAGTTAGT_lane-001-chunk-0002.fastq.gz'
+    p = ProcessFastQBarCodes()
+    p.read_standard_barcodes(barcode_filename)
+    p.process_fastq(fastq_filename, max_read_pairs=1000000)
