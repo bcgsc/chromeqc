bf3cde598bfba0e964579424b700c3b73d3a5daf
Chen Yang <cheny@bcgsc.ca>
2017-10-21 15:46:45 -0700

    Two separate files for longranger and chromeqc

--- multiqc/preprocess.sh
+++ multiqc/preprocess.sh
@@ -5,7 +5,8 @@ all_samples=`ls | grep '_lrbasic'`
 for sample in $all_samples;
 do
     sample_name=`basename $sample _lrbasic`
-    echo $sample_name
-    awk -v OFS=',' '{ print "'$sample_name'",$0; }' $sample/outs/summary.csv > $sample/outs/${sample_name}_basic_summary.csv
+    echo "SampleName" > tmp
+    echo $sample_name >> tmp
+    paste -d ',' tmp $sample/outs/summary.csv > $sample/outs/${sample_name}_basic_summary.csv
+    rm tmp
 done
-
